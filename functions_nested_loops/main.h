0a01e5e39bab9fffd87c4f6193b4162517188eed
yaxie <riveroalberto20z@gmail.com>
2025-10-16 11:52:09 -0400

    task 10

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,5 +9,6 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
+void times_table(void)
+int add(int a, int b);;
 #endif /* MAIN_H */
